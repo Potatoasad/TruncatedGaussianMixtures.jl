fc52ac575e69438956d75ddd0b2dfcd6121ccf48
Asad Hussain <asadhussain625@gmail.com>
2022-09-30 21:50:49 -0500

    Fixed Bug

--- src/fit.jl
+++ src/fit.jl
@@ -1,6 +1,6 @@
 function fit_gmm_2D(data, n_comps; bounds1=[0.0,1.0], bounds2=[0.0,1.0],tol=1e-16)
 
-	EM = ExpectationMaximization(data,n_comps)
+	EM = ExpectationMaximization(data,n_comps, bounds1=bounds1, bounds2=bounds2, tol=tol)
 	MAX_REPS = 1000
 	for i ∈ 1:10
 		update!(EM)
